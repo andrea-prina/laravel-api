7159345f471cd6afd9ab8a7f8cbdbbdaab9080ac
andrea-prina <and.prina@gmail.com>
2022-09-27 17:45:40 +0200

    Print posts title with vue

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,13 +1,44 @@
 <template>
     <div>
-        <h1>APP</h1>
+        <ul>
+            <li v-for="post in posts" :key="post['id']">
+                {{ post.title }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "App",
-    components: { }
+    components: {},
+
+    data : function(){
+        
+        return {
+            posts : [],
+        }
+    },
+
+    methods : {
+
+        getPosts : function(){
+
+            axios.get(`http://127.0.0.1:8000/api/posts`)
+            .then((result) => {
+                this.posts = result.data.results.data;
+            })
+            .catch((err) => {
+                console.warn(err);
+            })
+        }
+    },
+
+    created(){
+        this.getPosts();
+    }
 
 }
 </script>
